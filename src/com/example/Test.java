32c2aef0e0a28cef7e50c1364eef8bcc58e371c3
lokesh.jagtap009@gmail.com <you@example.com>
2024-02-08 04:42:29 +0530

    Test class created and commit in BranchX

---
+++ src/com/example/Test.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1="yes";
+		
+		if(s1=="yes")
+		{
+			System.out.println("Inside yes");
+		}
+		else
+		{
+			System.out.println("Inside no");
+		}
+	}
+
+}
